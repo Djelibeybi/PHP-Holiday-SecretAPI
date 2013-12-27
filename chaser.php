b64c1758092995f3c6503f2c55388a68cb4eb1b7
Avi Miller <avi.miller@gmail.com>
2013-12-27 18:02:12 +1100

    Set  = 0 to light up the first light in the string.

--- chaser.php
+++ chaser.php
@@ -14,45 +14,45 @@ include 'class.holiday.secretapi.php';
 // Need to pass the IP address or hostname of the Holiday on the command-line
 if ( isset($argv[1]) ) {
 
-	$holiday = new HolidaySecretAPI($argv[1]);
-	
-	while (true) {
+$holiday = new HolidaySecretAPI($argv[1]);
 
-		$r = rand(63, 255);
-		$g = rand(63, 255);
-		$b = rand(63, 255);
-		
-		// Now, based on another random value, squash one of these values
-		switch (rand(0,2)) {
-			
-			case 0:
-				$r = 0;
-			break;
-			case 1:
-				$g = 0;
-			break;
-			case 2:
-				$b = 0;
-			break;
-		}
-	
-		for ($globe; $globe < $holiday->NUM_GLOBES; $globe++) {
+while (true) {
 
-			// First turn off all the globes
-			$holiday->fill(0, 0, 0);
-			
-			// Now, turn on the next globe in sequence with the random colour
-			$holiday->setglobe($globe, $r, $g, $b);
-			
-			// Render the globes
-			$holiday->render();
-			
-			// Pause for audience applause
-			usleep(100000);
-			
-		}
+	$r = rand(63, 255);
+	$g = rand(63, 255);
+	$b = rand(63, 255);
+	
+	// Now, based on another random value, squash one of these values
+	switch (rand(0,2)) {
 		
-		unset($globe);
+		case 0:
+			$r = 0;
+		break;
+		case 1:
+			$g = 0;
+		break;
+		case 2:
+			$b = 0;
+		break;
+	}
+
+	for ($globe = 0; $globe < $holiday->NUM_GLOBES; $globe++) {
+
+		// First turn off all the globes
+		$holiday->fill(0, 0, 0);
+		
+		// Now, turn on the next globe in sequence with the random colour
+		$holiday->setglobe($globe, $r, $g, $b);
+		
+		// Render the globes
+		$holiday->render();
+		
+		// Pause for audience applause
+		usleep(100000);
+		
+	}
+	
+	unset($globe);
 			
 	}
 
